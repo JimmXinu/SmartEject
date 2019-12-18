f2c619264129dce4aa4397b405a05f15f06a6c8c
Jim Miller <retiefjimm@gmail.com>
2019-12-18 14:33:37 -0600

    Python37 for making translation messages.

--- makemessages.bat
+++ makemessages.bat
@@ -1,2 +1,2 @@
-c:\Python27\python.exe C:\Python27\Tools\i18n\pygettext.py  smarteject_plugin.py config.py __init__.py
+c:\Python37\python.exe C:\Python37\Tools\i18n\pygettext.py  smarteject_plugin.py config.py __init__.py
 mv messages.pot translations
