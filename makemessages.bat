93047484e335ee4225dae83049a87acfd9bb898a
Jim Miller <retiefjimm@gmail.com>
2022-01-11 13:06:14 -0600

    Update bat files.

--- makemessages.bat
+++ makemessages.bat
@@ -1,2 +1,2 @@
-c:\Python37\python.exe C:\Python37\Tools\i18n\pygettext.py  smarteject_plugin.py config.py __init__.py
+python C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python39\Tools\i18n\pygettext.py  smarteject_plugin.py config.py __init__.py
 mv messages.pot translations
